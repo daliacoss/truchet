6fb7258a50c98873d81f3c5195451f5c2f156c0b
Decky Coss <deckycoss@gmail.com>
2019-08-16 20:45:54 -0600

    refactoring of state atoms

--- src/truchet/core.cljs
+++ src/truchet/core.cljs
@@ -1,38 +1,46 @@
 (ns truchet.core
-  (:require [reagent.core :as reagent :refer [atom]]
+  (:require [reagent.core :as reagent :refer [atom rswap!]]
             [clojure.string :as string]
             [reanimated.core :as anim]
             [goog.string :as gstring]
             [goog.string.format]
             ))
 
-(defonce app-state (atom {:text "Hello world!"}))
-
 (def cell-size 100)
 
-(defn take-cycle
-  ([n xs]
-   (take-cycle n xs 0))
-  ([n xs i]
-   (take n (-> xs cycle (nthrest i)))))
+;(defn take-cycle
+;  ([n xs]
+;   (take-cycle n xs 0))
+;  ([n xs i]
+;   (take n (-> xs cycle (nthrest i)))))
 
-(defn cell [{:keys [x y r w]
-             :or {x 0, y 0, r 0, w cell-size}}]
+(defn cell-entry [{:keys [x y w r]}]
   (let [tl [(* x w) (* y w)]
         tr (update tl 0 + w)
         bl (update tl 1 + w)
-        br (update tr 1 + w)
-        orientations [[tl tr br]
-                      [tr br bl]
-                      [br bl tl]
-                      [bl tl tr]]
-        r (atom r)
-        points (map atom (repeat 6 0))
+        br (update tr 1 + w)]
+    {:r r
+     :tl tl
+     :w w
+     :coords [[tl tr br]
+              [tr br bl]
+              [br bl tl]
+              [bl tl tr]]}))
+
+(defn get-container []
+  (. js/document (getElementById "app")))
+
+(defn get-container-size []
+  (let [c (get-container)]
+    {:width c.clientWidth :height c.clientHeight}))
+
+(defn cell [{:keys [r coords]}]
+  (let [points (map atom (apply concat (get coords r)))
         anim-points (map anim/interpolate-to points)]
-    (fn [params]
+    (fn [{:keys [r coords tl w on-click] :as params}]
       (dorun
        ; flatten vector of vectors
-       (map reset! points (apply concat (get orientations (mod @r 4)))))
+       (map reset! points (apply concat (get coords r))))
       [:g
        [:polygon {:points (string/join " " (map deref anim-points))
                   :fill "black"}]
@@ -41,24 +49,62 @@
                :fill "none"
                :x (first tl)
                :y (second tl)
-               :on-click #(swap! r inc)}]])))
+               :on-click #(on-click (get params :key))}]])))
 
-(defn grid [{:keys [width height]}]
-  [:svg {:width "100%" :height "100%" :pointer-events "all"}
-   (let [rows (js/Math.ceil (/ height cell-size))
-         cols (js/Math.ceil (/ width cell-size))]
-     (for [y (range rows) x (range cols)]
-        [cell {:r (rand-int 4) :x x :y y :key (gstring/format"%d-%d" y x)}]))
-   ])
+(defn grid [params]
+  (fn [{:keys [rows cols cell-data on-cell-click]}]
+    [:svg {:width "100%" :height "100%" :pointer-events "all"}
+     (doall
+      (for [y (range rows) x (range cols)]
+           [cell
+            (assoc
+             (get cell-data [y x])
+             :key [y x]
+             :on-click on-cell-click)]))
+     ]))
 
-(defn render-grid []
-  (let [container (. js/document (getElementById "app"))
-        props {:width container.clientWidth :height container.clientHeight}]
-    (reagent/render-component [grid props] container)))
+(defn app []
+  ;(let [container (. js/document (getElementById "app"))
+  ;      width container.clientWidth
+  ;      height container.clientHeight
+  ;      rows (js/Math.ceil (/ height cell-size))
+  ;      cols (js/Math.ceil (/ width cell-size))]
+  (let [rows (atom 0)
+        cols (atom 0)
+        cell-states (atom {})
+
+        resize-and-fill-grid
+        (fn [{:keys [width height]}]
+          (let [new-rows (js/Math.ceil (/ height cell-size))
+                new-cols (js/Math.ceil (/ width cell-size))]
+            (->> (for [y (range new-rows) x (range new-cols)]
+                  [[y x]
+                   (if-let [c (get @cell-states [y x])]
+                           c
+                           (cell-entry {:x x :y y :w cell-size :r (rand-int 4)}))])
+                 (apply concat)
+                 (apply hash-map)
+                 (reset! cell-states))
+            (reset! rows new-rows)
+            (reset! cols new-cols)))
+
+        on-cell-click
+        (fn [k] 
+          (let [old-r (get-in @cell-states [k :r])]
+            (swap! cell-states assoc-in [k :r] (-> old-r inc (mod 4)))))]
+
+    (resize-and-fill-grid (get-container-size))
+    (. js/window (addEventListener
+                  "resize"
+                  #(resize-and-fill-grid (get-container-size))))
+    (fn []
+      [grid {:rows @rows
+             :cols @cols
+             :on-cell-click on-cell-click
+             :cell-data @cell-states}])))
 
 (defn start []
-  (. js/window (addEventListener "resize" render-grid))
-  (render-grid))
+  (reagent/render-component [app] (get-container)))
 
 (defn ^:export init []
   ;; init is called ONCE when the page loads
